3e38835c4fc1f2854ff15f3827eb2cd9cffa85b5
kumarashish33 <kumar3ashish32@gmail.com>
2026-08-12 20:46:35 +0530

    Add formatting

--- tests/test_training.py
+++ tests/test_training.py
@@ -1,10 +1,5 @@
-from src.train_model import (
-    load_data,
-    preprocess_data,
-    split_data,
-    train_model,
-    train_vectorizer,
-)
+from src.train_model import (load_data, preprocess_data, split_data,
+                             train_model, train_vectorizer)
 
 
 def test_model_training():
